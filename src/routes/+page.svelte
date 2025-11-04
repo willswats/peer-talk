4c6eb1daade835d1e0a7aec7e06beff00712f5aa
willswats <william@williamwatson.dev>
2025-11-04 00:10:47 +0000

    feat: add mediaDevices

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	interface constraints {
+		video: boolean;
+		audio: boolean;
+	}
+
+	onMount(() => {
+		const openMediaDevices = async (constraints: constraints) => {
+			return await navigator.mediaDevices.getUserMedia(constraints);
+		};
+
+		try {
+			const stream = openMediaDevices({ video: true, audio: true });
+			console.log('Got MediaStream:', stream);
+		} catch (error) {
+			console.error('Error accessing media devices.', error);
+		}
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
