c53e9343c350388db75ba4aaff941c7f2667e5b9
willswats <william@williamwatson.dev>
2026-02-25 13:13:49 +0000

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Peer Talk dev";
+
+  # Flake inputs
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # unstable Nixpkgs
+
+  # Flake outputs
+  outputs =
+    { self, ... }@inputs:
+
+    let
+      # The systems supported for this flake
+      supportedSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShellNoCC {
+            # The Nix packages provided in the environment
+            # Add any you need here
+            packages = with pkgs; [
+              nodejs
+              pnpm
+              svelte-language-server
+            ];
+
+            # Set any environment variables for your dev shell
+            env = { };
+
+            # Add any shell logic you want executed any time the environment is activated
+            shellHook = '''';
+          };
+        }
+      );
+    };
+}
