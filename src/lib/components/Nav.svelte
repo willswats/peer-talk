df93023158e129c0d229fc720abf1c004ec78d5a
willswats <william@williamwatson.dev>
2026-03-05 11:23:38 +0000

    feat: add nav styles

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -2,13 +2,59 @@
 	import { resolve } from '$app/paths';
 	import { v4 as uuidv4 } from 'uuid';
 	import { userState } from '$lib/state.svelte';
+	import { page } from '$app/state';
+
+	let uuid = uuidv4();
+
+	const userOnPage = (url: string) => page.url.pathname === url;
 </script>
 
 <nav>
-	<a href={resolve('/')}>Home</a>
-	{#if userState.joinedRoom && userState.roomId}
-		<a href={resolve(`/room/${userState.roomId}`)}>Room</a>
-	{:else}
-		<a href={resolve(`/room/${uuidv4()}`)}>Create Room</a>
-	{/if}
+	<section class="nav-menu">
+		<a class:nav-menu-a-enabled={userOnPage(`/`)} href={resolve('/')}>Home</a>
+		{#if userState.joinedRoom && userState.roomId}
+			<a
+				class:nav-menu-a-enabled={userOnPage(`/room/${userState.roomId}`)}
+				href={resolve(`/room/${userState.roomId}`)}>Room</a
+			>
+		{:else}
+			<a class:nav-menu-a-enabled={userOnPage(`/room/${uuid}`)} href={resolve(`/room/${uuid}`)}
+				>Create Room</a
+			>
+		{/if}
+	</section>
 </nav>
+
+<style>
+	nav {
+		display: flex;
+		width: 100%;
+		height: 2rem;
+		background-color: #2a2a2a;
+		border-bottom: 1px solid #4a4a4a;
+		overflow: hidden;
+	}
+
+	.nav-menu {
+		display: flex;
+		text-wrap: nowrap;
+	}
+
+	.nav-menu a {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 1rem;
+		font-size: 1.2rem;
+		border-right: 1px solid #4a4a4a;
+		transition: background-color 0.1s;
+	}
+
+	.nav-menu a:hover {
+		background-color: #3a3a3a;
+	}
+
+	.nav-menu-a-enabled {
+		background-color: #3a3a3a;
+	}
+</style>
