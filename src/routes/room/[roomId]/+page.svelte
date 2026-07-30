bfe1ce4ee1be0391f4af6eba5fae3adb6a3a3d81
willswats <william@williamwatson.dev>
2026-07-30 16:46:40 +0100

    feat: style invalid room

--- src/routes/room/[roomId]/+page.svelte
+++ src/routes/room/[roomId]/+page.svelte
@@ -18,10 +18,37 @@
 
 {#if !roomValid}
 	<main>
-		<p>Invalid room</p>
+		<p>Invalid room, are you sure the room id is correct?</p>
 	</main>
 {:else if userState.joinedRoom}
 	<Room />
 {:else}
 	<JoinSettings />
 {/if}
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		flex-grow: 1;
+		margin: 1rem;
+		animation: fade-in 0.8s ease-out forwards;
+	}
+
+	p {
+		background-color: var(--bg-secondary);
+		padding: 1rem;
+		border-radius: var(--border-radius-normal);
+		border: 1px solid var(--border);
+		font-size: 1.4rem;
+	}
+
+	@media screen and (max-width: 768px) {
+		p {
+			font-size: 1rem;
+		}
+	}
+</style>
