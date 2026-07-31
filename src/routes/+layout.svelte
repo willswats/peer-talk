7b0352ea04f51e01e6061d18371745a2daffc85a
willswats <william@williamwatson.dev>
2026-07-31 15:48:51 +0100

    fix: favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import faviconApple from '$lib/assets/favicon/apple-touch-icon.png';
+	import faviconThirtyTwo from '$lib/assets/favicon/favicon-32x32.png';
+	import faviconSixteen from '$lib/assets/favicon/favicon-16x16.png';
+	import webManifest from '$lib/assets/favicon/site.webmanifest';
+
 	import Nav from '$lib/components/Nav.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="apple-touch-icon" sizes="180x180" href={faviconApple} />
+	<link rel="icon" type="image/png" sizes="32x32" href={faviconThirtyTwo} />
+	<link rel="icon" type="image/png" sizes="16x16" href={faviconSixteen} />
+	<link rel="manifest" href={webManifest} />
 </svelte:head>
 
 <Nav />
